0b4c3c050dc76053f0495a3db376e31d8f4b7509
huangdijia <huangdijia@gmail.com>
2018-07-19 16:10:16 +0800

    support datx download

--- src/Console/IpipDownloadCommand.php
+++ src/Console/IpipDownloadCommand.php
@@ -2,6 +2,7 @@
 
 namespace Huangdijia\Ipip\Console;
 
+use Exception;
 use Illuminate\Console\Command;
 use ZipArchive;
 
@@ -17,23 +18,37 @@ class IpipDownloadCommand extends Command
     public function handle()
     {
         $this->cookiejar = storage_path('ipip.cookie');
-        $savepath        = storage_path('app/public/17monipdb.zip');
+        $zip_file        = storage_path('app/public/17monipdb.zip');
+        $datx_file       = storage_path('app/public/17monipdb/17monipdb.datx');
+
+        if (!config('ipip.auth.mail') || !config('ipip.auth.pass')) {
+            throw new Exception('Please set ipip.auth.mail && ipip.auth.pass');
+            return;
+        }
 
         // login ipip
+        $this->info('logining ipip.net');
         $this->login(
             config('ipip.auth.mail'),
             config('ipip.auth.pass')
         );
-        $this->download($savepath);
+        $this->info('logined');
+
+        // downling
+        $this->info('downloading');
+        $this->download($zip_file);
+        if (!is_file($zip_file)) {
+            throw new Exception('Download faild!');
+        }
+        $this->info('download success! path: ' . $zip_file);
 
         // unzip
-        $zip = new ZipArchive;
-        if ($zip->open($savepath)) {
-            $zip->extractTo(storage_path('app/public'));
+        $this->info('extracting');
+        $this->extract($zip_file);
+        if (!is_file($datx_file)) {
+            throw new Exception("Extract faild", 1);
         }
-
-        // report
-        $this->info('Download success! path:' . storage_path('app/public/17monipdb/17monipdb.datx'));
+        $this->info('extracted success! path:' . $datx_file);
     }
 
     private function login($mail = '', $pass = '')
@@ -51,19 +66,46 @@ class IpipDownloadCommand extends Command
         curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
-        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookiejar);
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new Exception(curl_error($ch));
+        }
+        if (
+            stripos($response, '请使用邮箱登录')
+            || stripos($response, '登录失败')
+        ) {
+            throw new Exception('Login faild');
+        }
+        curl_close($ch);
     }
 
-    private function download($savepath)
+    private function download($zip_file)
     {
-        $fp = fopen($savepath, 'w+');
+        $fp = fopen($zip_file, 'w+');
         $ch = curl_init(self::IPIP_DOWNLOAD_URL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookiejar);
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new Exception(curl_error($ch));
+        }
         fclose($fp);
+    }
+
+    private function extract($zip_file)
+    {
+        // unzip
+        try {
+            $zip = new ZipArchive;
+            if ($zip->open($zip_file) === true) {
+                $zip->extractTo(storage_path('app/public/17monipdb'));
+                $zip->close();
+            }
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 }
