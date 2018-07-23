465fca0b475619a476bb22865b959e4d7bb332ea
huangdijia <huangdijia@gmail.com>
2018-07-23 15:11:48 +0800

    update

--- src/Facades/BaseStation.php
+++ src/Facades/BaseStation.php
@@ -4,6 +4,11 @@ namespace Huangdijia\Ipip\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array find(string $ip)
+ * @see \ipip\datx\BaseStation
+ */
+
 class BaseStation extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Facades/City.php
+++ src/Facades/City.php
@@ -4,6 +4,11 @@ namespace Huangdijia\Ipip\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array find(string $ip)
+ * @see \ipip\datx\City
+ */
+
 class City extends Facade
 {
     protected static function getFacadeAccessor()
