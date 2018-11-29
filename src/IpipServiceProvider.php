5b80e5ba955ff54709f3dc29ba35d046cfd1c4c4
huangdijia <huangdijia@gmail.com>
2018-11-29 14:43:47 +0800

    support laravel 5.7

--- src/IpipServiceProvider.php
+++ src/IpipServiceProvider.php
@@ -2,51 +2,66 @@
 
 namespace Huangdijia\Ipip;
 
+use Huangdijia\Ipip\Console\BaseStationFindCommand;
+use Huangdijia\Ipip\Console\CityFindCommand;
+use Huangdijia\Ipip\Console\DistrictFindCommand;
+use Huangdijia\Ipip\Console\DownloadCommand;
 use Illuminate\Support\ServiceProvider;
+use ipip\datx\BaseStation;
 use ipip\datx\City;
 use ipip\datx\District;
-use ipip\datx\BaseStation;
 
 class IpipServiceProvider extends ServiceProvider
 {
-    protected $defer = true;
+    protected $defer    = true;
     protected $commands = [
-        'Huangdijia\\Ipip\\Console\\IpipCityFindCommand',
-        'Huangdijia\\Ipip\\Console\\IpipDistrictFindCommand',
-        'Huangdijia\\Ipip\\Console\\IpipBaseStationFindCommand',
-        'Huangdijia\\Ipip\\Console\\IpipDownloadCommand',
+        CityFindCommand::class,
+        DistrictFindCommand::class,
+        BaseStationFindCommand::class,
+        DownloadCommand::class,
     ];
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__.'/../config/config.php' => config_path('ipip.php')]);
+            $this->publishes([__DIR__ . '/../config/config.php' => config_path('ipip.php')]);
         }
     }
 
     public function register()
     {
-        $this->app->singleton('ipip.city', function () {
+        $this->app->singleton(City::class, function () {
             $path = config('ipip.datx.city');
             return new City($path);
         });
-        $this->app->singleton('ipip.district', function () {
+        $this->app->alias(City::class, 'ipip.city');
+
+        $this->app->singleton(District::class, function () {
             $path = config('ipip.datx.district');
             return new District($path);
         });
-        $this->app->singleton('ipip.basestation', function () {
+        $this->app->alias(District::class, 'ipip.district');
+
+        $this->app->singleton(BaseStation::class, function () {
             $path = config('ipip.datx.basestation');
             return new BaseStation($path);
         });
+        $this->app->alias(BaseStation::class, 'ipip.basestation');
+
         $this->commands($this->commands);
     }
 
     public function provides()
     {
         return [
+            City::class,
             'ipip.city',
+
+            District::class,
             'ipip.district',
-            'ipip.basestation'
+
+            BaseStation::class,
+            'ipip.basestation',
         ];
     }
 }
